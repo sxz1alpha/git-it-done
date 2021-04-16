e41b8017d0db09b76bb4a71cc9e79e46c2d2d0d2
UCDT <adrian.j.ulibarri@gmail.com>
2021-04-15 18:44:03 -0600

    finished making functions to pull data and append the contents to the page

---
+++ assets/js/single.js
@@ -0,0 +1,67 @@
+
+// finds the html element all of the data will be displayed inside
+var issueContainerEl = document.querySelector("#issues-container");
+
+// calles tthe github api for open issues
+var getRepoIssues = function(repo) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+  
+    // make a get request to url
+    fetch(apiUrl).then(function(response) {
+      // request was successful
+      if (response.ok) {
+        response.json().then(function(data) {
+          displayIssues(data);
+        });
+      }
+      else {
+        console.log(response);
+        alert("There was a problem with your request!");
+      }
+    });
+  };
+
+//appends issues to the page
+var displayIssues = function(issues) {
+    if (issues.length === 0) {
+        issueContainerEl.textContent = "This repo has no open issues!";
+        return;
+    }
+    
+    for (var i = 0; i < issues.length; i++) {
+        // create a link element to take users to the issue on github
+        var issueEl = document.createElement("a");
+        issueEl.classList = "list-item flex-row justify-space-between align-center";
+        issueEl.setAttribute("href", issues[i].html_url);
+        issueEl.setAttribute("target", "_blank");
+        
+        // create span to hold issue title
+        var titleEl = document.createElement("span");
+        titleEl.textContent = issues[i].title;
+        
+    
+        // append to container
+        issueEl.appendChild(titleEl);
+
+        // create a type element
+        var typeEl = document.createElement("span");
+
+        // check if issue is an actual issue or a pull request
+        if (issues[i].pull_request) {
+        typeEl.textContent = "(Pull request)";
+        }
+        else {
+        typeEl.textContent = "(Issue)";
+        }
+
+        // append to container
+        issueEl.appendChild(typeEl);
+    
+        // append to the dom
+        issueContainerEl.appendChild(issueEl);
+    }
+};
+
+
+getRepoIssues("sxz1alpha/Terran-project");
